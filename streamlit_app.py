882893bd23a8049955f852c3e5a3c1ec25b21fc1
Muhammed <79092044+Badanga@users.noreply.github.com>
2023-11-22 13:57:03 +0100

    Update streamlit_app.py
    rerange

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 import pandas
-import requests
-streamlit.header("Fruityvice Fruit Advice!")
+
 
 streamlit.title("My Parents New Healthy Diner")
 
@@ -20,6 +19,9 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+import requests
+streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
