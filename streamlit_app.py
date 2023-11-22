43ff6ff700e1e6a38bb15eefb12d173e5f410caa
Muhammed <79092044+Badanga@users.noreply.github.com>
2023-11-22 09:41:04 +0100

    Update streamlit_app.py
    adjustetd header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit
 
-streamlit.title("My parents New Helathy Diner")
-streamlit.header("Breakfast Menu")
-streamlit.text("🥣 Omego 3 and Blueberry Oatmeal") 
-streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
-streamlit.text("🐔 Hard-Boiled Free-range egg")
-streamlit.text("🥑🍞 Avocado Toast") 
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
