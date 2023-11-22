a51ca2bc068be7cb6e5d7222a7a3fcc0f56bebe8
Muhammed <79092044+Badanga@users.noreply.github.com>
2023-11-22 09:57:53 +0100

    Update streamlit_app.py
    added index

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,17 @@
 import streamlit
 import pandas
 
+streamlit.title("My Parents New Healthy Diner")
+
 streamlit.header("Breakfast Menu")
 streamlit.text("🥣 Omego 3 and Blueberry Oatmeal") 
 streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-range egg")
 streamlit.text("🥑🍞 Avocado Toast") 
 
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
-streamlit.dataframe(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index("Fruit")
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
