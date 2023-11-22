87e6ac713ad9be215f28e45676831d04d9aedce8
Muhammed <79092044+Badanga@users.noreply.github.com>
2023-11-22 09:41:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header("Breakfast Menu")
+streamlit.text("🥣 Omego 3 and Blueberry Oatmeal") 
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-range egg")
+streamlit.text("🥑🍞 Avocado Toast") 
