77eb85b6acf197df17c1fdad944f444b2153f96b
Muhammed <79092044+Badanga@users.noreply.github.com>
2023-11-22 16:31:03 +0100

    try catch block

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,16 +27,26 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice )
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+except URLErrors as e:
+  streamlit.error()
+
+#streamlit.write('The user entered ', fruit_choice)
+
 #streamlit.text(fruityvice_response.json()) deleted
 
 # take the json response an normalize it 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
 # show it on screen
-streamlit.dataframe(fruityvice_normalized)
+
 
 
 streamlit.stop()
