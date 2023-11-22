d5c43fbc1ea1d646ece82173b1c16daa9817d331
Muhammed <79092044+Badanga@users.noreply.github.com>
2023-11-22 09:39:57 +0100

    Update streamlit_app.py
    added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("My parents New Helathy Diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("🥣 Omego 3 and Blueberry Oatmeal") 
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-range egg")
+streamlit.text("🥑🍞 Avocado Toast") 
